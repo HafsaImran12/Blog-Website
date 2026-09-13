10978efcd8a15e72d96a98a4f739e79770317d9d
Hafsa Imran <hafsaemran1712@gmail.com>
2026-09-13 16:22:07 +0500

    Update

--- pages/home.js
+++ pages/home.js
@@ -1,12 +1,18 @@
-
-
+const userName = document.querySelector("#userName")
+const userEmail = document.querySelector("#userEmail")
 const logout = document.querySelector("#logout")
 
+let getUser = async () => {
+    const { data: { user } } = await supabase.auth.getUser()
+    console.log(user.email);
 
-logout.addEventListener("click",async(event)=>{
+    userName.innerHTML = data.user.email
+}
+
+logout.addEventListener("click", async (event) => {
     event.preventDefault()
     const { error } = await client.auth.signOut()
-    if(error){
+    if (error) {
         console.log(error);
     }
     window.location.href = "../index.html"
